e7c8e7bcd4b139e7fecb36e34e4c945648921a18
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-11-02 23:06:13 -0500

    up

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,15 +1,13 @@
 // firebaseConfig.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-storage.js'; // Include storage
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-auth.js";
+import { getStorage } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-storage.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js'; // Updated to match other imports
 
 // Firebase configuration object
 const firebaseConfig = {
- //   apiKey: import.meta.env.VITE_FIREBASE_API_KEY, // Use environment variables for sensitive data
- apiKey: "AIzaSyC2bb6osv0jnvpnETCVoG6bvBynGGsOVaw",
-
- authDomain: "raw-photography-12616.firebaseapp.com",
+    apiKey: "AIzaSyC2bb6osv0jnvpnETCVoG6bvBynGGsOVaw",
+    authDomain: "raw-photography-12616.firebaseapp.com",
     projectId: "raw-photography-12616",
     storageBucket: "raw-photography-12616.appspot.com",
     messagingSenderId: "1078385378836",
@@ -24,15 +22,14 @@ let storage;
 let auth;
 
 try {
-     app = initializeApp(firebaseConfig);
- auth = getAuth(app);
-
-    db = getFirestore(app);
-    storage = getStorage(app); // Initialize storage
+    app = initializeApp(firebaseConfig);
+    auth = getAuth(app); // Initialize auth
+    db = getFirestore(app); // Initialize Firestore
+    storage = getStorage(app); // Initialize Storage
     console.log("Firebase initialized successfully.");
 } catch (error) {
     console.error("Error initializing Firebase:", error);
 }
 
-// Export Firestore and Storage instances for use in other modules
+// Export Firestore, Storage, and Auth instances for use in other modules
 export { db, storage, auth };
